254298850f613cc20a007d277b23f9ac7ddab4da
Suren <surenatoyan93@gmail.com>
2016-12-20 01:37:43 +0400

    Happy New year (2017)

---
+++ happyNewYear2017.js
@@ -0,0 +1,32 @@
+
+												       /****/
+											                /**/
+													//\\
+												       //| \\
+												      //||| \\
+												     //||||| \\
+											            //||||||| \\
+												   //|||JS|||| \\
+												  //|||NODE|||| \\
+												 //||||||||||||| \\
+											        //||||||||||||||| \\
+											       //||||||||||||||||| \\
+											      const year = (2016 + 1); 
+											     class HappyNewYearCoders {
+											    constructor ( year, wish ) {
+											   this.year=year;this.wish=wish;
+										          this.Author =Symbol}getMessage()
+										         {return `Happy new :: ${this.year}
+										        Year to all :*ProGraMMerS*: and :IT:
+									               specialists.:::${this.wich}:::. From :
+										      :${this.ident()[this.Author]}`}ident() {
+										      return {[this.Author]:'SurenAt'}}alert(){
+										     console.log('Message: ',this.getMessage());
+										    }}; const wish=( happy => { return function*
+										               _new(){return function
+										               *year() { return `Love
+											       ^what you :::::do:::::
+					                                                       ::`;} } } )()().next()
+	                                                                                       .value().next().value;   
+										          (new HappyNewYearCoders(year, (wish)))
+										                      .alert();
